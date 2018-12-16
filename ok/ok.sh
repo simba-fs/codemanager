4609158a1ed4b4dba8c9cede0460394cbfe302ae
wade <wade.fs@gmail.com>
2018-12-16 16:55:18 +0800

    modified ok.sh by wade

--- ok/ok.sh
+++ ok/ok.sh
@@ -2,17 +2,17 @@
 [ -z $1 ] && message="change something" || message=$1
 [ -z $2 ] && respository="origin" || respository=$2
 #echo $message $respository
-add="git add ."
-commit="git commit -m '$message'"
 push="git push "$respository
-echo "> "$add
-echo "> "$commit
-echo "> "$push
-echo "> [Y/n]? "
+cat <<HERE_DOCUMENT
+  git add .
+  git commit -m "$message"
+  git push "$respository"
+  [Y/n]?
+HERE_DOCUMENT
 read ans
-#echo "> "$ans
+echo "> $ans"
 if [ "x${ans}" = "xy" ] || [ "x${ans}" = "xY" ] || [ "x${ans}" = "x" ]; then
-#	$add
-	`$commit`
-#	$push
+  git add --all .
+  git commit -m "$message"
+  git push "$respository"
 fi
