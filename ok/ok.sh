881166462aaa58e306a8a9cca7686989e2aab83e
simba-fs <simba.fs@gamil.com>
2018-12-16 16:01:14 +0800

    ok.sh

--- ok/ok.sh
+++ ok/ok.sh
@@ -1,8 +1,9 @@
+#set -x
 [ -z $1 ] && message="change something" || message=$1
 [ -z $2 ] && respository="origin" || respository=$2
 #echo $message $respository
 add="git add ."
-commit="git commit -m \""$message"\""
+commit="git commit -m '$message'"
 push="git push "$respository
 echo "> "$add
 echo "> "$commit
@@ -11,7 +12,7 @@ echo "> [Y/n]? "
 read ans
 #echo "> "$ans
 if [ "x${ans}" = "xy" ] || [ "x${ans}" = "xY" ] || [ "x${ans}" = "x" ]; then
-	$add
-	$commit
-	$push
+#	$add
+	`$commit`
+#	$push
 fi
