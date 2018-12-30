9f0c79fcb97b20c890363974afea75360338332a
simba <simba.fs@gmail.com>
2018-12-30 21:11:52 +0800

    change something

---
+++ fin/fin.sh
@@ -0,0 +1,28 @@
+
+if [ $#=1 ];then
+	message="change something"
+else
+	message=$2
+fi
+
+if [ "x$1" = "x" ];then
+	echo "error:You can't miss [file name]"
+else
+cat <<tag1 
+> mv $1.* ../finish/
+> rm $1
+> cd ../
+> vi README.md
+> ok $message
+> [Y/n]?
+tag1
+	read ans
+	if [ "x$ans" = "xy" ] || [ "x$ans" = "xY" ] || [ "x$ans" = "x" ];then
+		mv $1.* ../finish/
+		rm $1
+		cd ../
+		vi README.md
+		ok $message
+		echo successed!!
+	fi
+fi
