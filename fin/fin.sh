ef22a99b7e9e10c588268ee89008dd74f5f2701e
simbafs <simba.fs@gmail.com>
2018-12-31 14:11:24 +0800

    fin.sh

--- fin/fin.sh
+++ fin/fin.sh
@@ -8,9 +8,20 @@ fi
 if [ "x$1" = "x" ];then
 	echo "error:You can't miss [file name]"
 else
+if [[ -f "$1.c" ]] ;then
+	file="c"
+	echo "> mv $1.c ../finish"
+else
+	file="cpp"
+	echo "> mv $1.cpp ../finish"
+fi
+if [[ -f "$1.data" ]] ;then
+	data=1
+	echo "> $1.data ../data"
+else
+	data=0
+fi
 cat <<tag1 
-> mv $1.* ../finish/
-> rm $1
 > cd ../
 > vi README.md
 > ok $message
@@ -19,6 +30,9 @@ tag1
 	read ans
 	if [ "x$ans" = "xy" ] || [ "x$ans" = "xY" ] || [ "x$ans" = "x" ];then
 		mv $1.* ../finish/
+		if [[ $data = 1 ]] ;then
+			mv $1.data ../data
+		fi
 		rm $1
 		cd ../
 		vi README.md
